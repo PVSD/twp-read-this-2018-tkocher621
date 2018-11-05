e4f1faea83e3e0e8e05c09cb6b34769f7799335a
tk304 <tk304@pvsd.org>
2018-11-05 11:20:40 -0500

    im already tracer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+
+        String filePath = System.getProperty("user.dir") + "/src/Imports.txt";
+        File file = new File(filePath);
+        Scanner sf = new Scanner(file);
+
+        int lineCount = 0;
+
+        while (sf.hasNext())
+            System.out.println(sf.next());
+
+
+
     }
 }
